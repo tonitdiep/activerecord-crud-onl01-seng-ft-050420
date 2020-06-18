4fb2877ce5fb05e5467169384083e1e26912da15
Toni  Diep <adiep123@gmail.com>
2020-06-18 01:06:50 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -73,7 +73,8 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie.title = "Even Awesomer Flick"  
-  movie.save   #not passing/undefined method 'save'
+  # movie.update(title: "Even Awesomer Flick")  #also works
+  movie.save  
 end
 
 def can_update_using_update_method
